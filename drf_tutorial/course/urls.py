5214e24c3fca039d453ebe11178625a4102ccf48
Hellen <cxsting@gmail.com>
2025-03-05 23:52:35 +0800

    API_View類視圖方式

--- drf_tutorial/course/urls.py
+++ drf_tutorial/course/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from course import views
+
+# 引入視圖函數或視圖類
+from course.views import course_list, course_detail, CourseListView, CourseDetailView
+
 urlpatterns = [
-    # 函數式接口 Function Base View
-    path('fbv/list/', views.course_list, name='fbv-list'),
-    path('fbv/detail/<int:pk>/', views.course_detail, name='fbv-detail') # 要調用哪個方法應該在http請求中做出區別，而不是在url中設定
+    # 函數式接口 Function Based View
+    path('fbv/list/', course_list, name='fbv-list'),
+    path('fbv/detail/<int:pk>/', course_detail, name='fbv-detail'), # 要調用哪個方法應該在http請求中做出區別，而不是在url中設定
+
+    # 類視圖接口 Class Based View
+    path('cbv/list/', CourseListView.as_view(), name='cbv-list'),
+    path('cbv/detail/<int:pk>/', CourseDetailView.as_view(), name='cbv-detail'),
 ]
--- drf_tutorial/course/views.py
+++ drf_tutorial/course/views.py
@@ -1,9 +1,9 @@
-from .serializers import CourseSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, APIView
+from rest_framework.exceptions import NotFound, ValidationError
 from .models import Course
-from django.contrib.auth.models import User
+from .serializers import CourseSerializer
 
 """
 課程API接口的四種寫法
@@ -18,33 +18,35 @@ from django.contrib.auth.models import User
 
 
 """函數式 Function Base View """
-@api_view(["GET","POST"])
-def course_list(request): 
+
+
+@api_view(["GET", "POST"])
+def course_list(request):
     """
     獲取所有課程訊息或新增一個課程
 
     Args:
         resquest (_type_): 接收用戶端傳來的請求
-    """ 
+    """
     # GET請求:獲取所有課程信息並回傳給用戶端
     if request.method == 'GET':
-        courses = Course.objects.all() # 獲取所有課程
-        s = CourseSerializer(instance=courses,many=True) # 序列化模型實例轉成Json格式
-        return Response(s.data, status=status.HTTP_200_OK) 
+        courses = Course.objects.all()  # 獲取所有課程
+        s = CourseSerializer(instance=courses, many=True)  # 序列化模型實例轉成Json格式
+        return Response(s.data, status=status.HTTP_200_OK)
 
-    else: # POST請求: 接受用戶傳來的數據並新增一個課程
+    else:  # POST請求: 接受用戶傳來的數據並新增一個課程
         s = CourseSerializer(data=request.data)  # 反序列化
         # 進行資料驗證
         if s.is_valid():
             # 驗證成功則創建並保存模型實例
-            s.save(teacher=request.user) # 將講師欄設定為當前登入用戶
+            s.save(teacher=request.user)  # 將講師欄設定為當前登入用戶
             return Response(s.data, status=status.HTTP_201_CREATED)
         # 驗證失敗則返回錯誤訊息
         return Response(s.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", 'PATCH', 'DELETE'])
-def course_detail(request, pk): # pk為url裡面傳遞過來的
+def course_detail(request, pk):  # pk為url裡面傳遞過來的
     """
     查詢指定的課程、更新/部分更新課程訊息/刪除指定課程
     Args:
@@ -52,45 +54,102 @@ def course_detail(request, pk): # pk為url裡面傳遞過來的
         pk (_type_): 指定課程的id
     """
     try:
-         course = Course.objects.get(pk=pk) # 透過ID查詢資料庫中的指定的課程訊息
+        course = Course.objects.get(pk=pk)  # 透過ID查詢資料庫中的指定的課程訊息
 
-    except Course.DoesNotExist: # get方法可能會查無，故要處理異常
-        return Response(data={"msg":"沒有此課程訊息"}, status=status.HTTP_404_NOT_FOUND)
-        
+    except Course.DoesNotExist:  # get方法可能會查無，故要處理異常
+        return Response(data={"msg": "沒有此課程訊息"}, status=status.HTTP_404_NOT_FOUND)
+
     else:
         # GET請求:查詢並返回指定課程訊息
         if request.method == 'GET':
-            s = CourseSerializer(instance=course) # 序列化指定模型實例轉成Json格式
+            s = CourseSerializer(instance=course)  # 序列化指定模型實例轉成Json格式
             return Response(s.data, status=status.HTTP_200_OK)
 
-        # PUT請求:更新指定課程的所有訊息    
+        # PUT請求:更新指定課程的所有訊息
         elif request.method == "PUT":
-            # 反序列化
-            s = CourseSerializer(instance=course, data=request.data) # 要更新的模型為course，更新的資料為用戶傳來的request.data
+            # 要更新的模型為course，更新的資料為用戶傳來的request.data
+            s = CourseSerializer(instance=course, data=request.data)  # 反序列化
+
             # 驗證資料
             if s.is_valid():
                 # 驗證成功則updat保存e並到資料庫
-                s.save() # 因更新現有資料，講師欄之前就默認為當前登入用戶，故不用像POST請求一樣新增
-                return Response(data=s.data, status=status.HTTP_201_CREATED)
-            # 驗證失敗則返回錯誤訊息
-            return Response(data=s.errors, status=status.HTTP_400_BAD_REQUEST) 
-        
-        # PETCH請求:僅更新指定的欄位
-        elif request.method == "PATCH":
-            s = CourseSerializer(instance=course, data=request.data, partial=True) # 反序列化
-            if s.is_valid():
-                s.save()
-                return Response(data=s.data, status=status.HTTP_201_CREATED)
+                s.save()  # 因更新現有資料，講師欄之前就默認為當前登入用戶，故不用像POST請求一樣新增
+                return Response(data=s.data, status=status.HTTP_200_OK)
             # 驗證失敗則返回錯誤訊息
             return Response(data=s.errors, status=status.HTTP_400_BAD_REQUEST)
-        
+
+        # PETCH請求:僅更新指定的欄位
+        elif request.method == "PATCH":
+            s = CourseSerializer(
+                instance=course, data=request.data, partial=True)  # 反序列化
+            if s.is_valid():
+                s.save()
+                return Response(data=s.data, status=status.HTTP_200_OK)
+            # 驗證失敗則返回錯誤訊息
+            return Response(data=s.errors, status=status.HTTP_400_BAD_REQUEST)
+
         # DELETE請求:刪除指定課程
         else:
-            course.delete() # 直接刪除該模型實例
-            return Response(status=status.HTTP_204_NO_CONTENT)            
+            course.delete()  # 直接刪除該模型實例
+            return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-# """類視圖 Class Base View """
+"""類視圖 Class Based View """
+class CourseListView(APIView):
+    def get(self, request):
+        """獲取所有課程資料"""
+        courses = Course.objects.all()
+        s = CourseSerializer(instance=courses, many=True)
+        return Response(data=s.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        """新增一個課程"""
+        s = CourseSerializer(data=request.data)  # 接收用戶傳來的資料進行反序列化
+        if s.is_valid():  # 驗證資料
+            s.save(teacher=self.request.user)  # 因為是類，故記得加self
+            return Response(data=s.data, status=status.HTTP_201_CREATED)
+        return Response(data=s.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CourseDetailView(APIView):
+    @staticmethod
+    def get_obj(pk):
+        """獲取指定的課程模型實例"""
+        try:
+            return Course.objects.get(pk=pk)
+        except Course.DoesNotExist:
+            raise NotFound("Course not found")  # 拋出NotFoun異常
+
+    def get(self, request, pk):
+        """查詢指定課程"""
+        course = self.get_obj(pk=pk)  # 獲取模型實例
+        s = CourseSerializer(instance=course)
+        return Response(data=s.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        """更新指定課程"""
+        course = self.get_obj(pk=pk)  # 獲取模型實例
+        s = CourseSerializer(instance=course, data=request.data)  # 序列化模型實例
+        if s.is_valid():
+            s.save()
+            return Response(data=s.data, status=status.HTTP_200_OK)
+        raise ValidationError(s.errors)  # 抛出ValidationError異常
+
+    def patch(self, request, pk):
+        """更新指定課程（部分更新）"""
+        course = self.get_obj(pk=pk)
+        s = CourseSerializer(instance=course, data=request.data, partial=True)  # 反序列化
+        if s.is_valid():
+            s.save()
+            return Response(data=s.data, status=status.HTTP_200_OK)
+        raise ValidationError(s.errors)  # 抛出ValidationError异常
+
+    def delete(self, request, pk):
+        """刪除指定課程"""
+        course = self.get_obj(pk=pk)
+        course.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # """通用類視圖 Gernic Base View"""
 
